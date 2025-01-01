e8c1a430a8b1a2833a23fd1c244920336e1b11ca
Abhay <abhay.fernandes@protonmail.com>
2024-12-31 21:44:04 -0500

    feat: add uploading diffs

--- cmd/crev/main.go
+++ cmd/crev/main.go
@@ -1,28 +1,54 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+
+	"github.com/AbhayFernandes/review_tool/pkg/proto"
 )
 
 func main() {
-	// serverAddr := flag.String(
-	//     "server", "localhost:8080",
-	//     "The server address in the form of host:port",
-	// )
-	// flag.Parse()
+    serverAddr := flag.String(
+        "server", "localhost:8080",
+        "The server address in the form of host:port",
+    )
 
-	// client, ctx, conn, cancel := getClient(serverAddr)
-	// defer conn.Close()
-	// defer cancel()
+    user := flag.String(
+        "user", "ferna355",
+        "Your MSU NetID without the @msu.edu. Ex: ferna355",
+    )
 
-	// fmt.Println(sayHello(client, ctx))
+    flag.Parse()
 
-	currentDir := getCurrentDir()
-	repository := getRepository(currentDir)
+    client, ctx, conn, cancel := getClient(serverAddr)
+    defer conn.Close()
+    defer cancel()
 
-	diffs, err := getPatchDiffs(repository)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(diffs)
+    diff, err := getDiffs()
+    if (err != nil) {
+        fmt.Println("There was an error getting diffs. Are you in a git repo? Is there a commit to upload?")
+    }
+
+    _, err = client.UploadDiff(ctx, &proto.UploadDiffRequest{
+        Diff: diff,
+        User: *user,
+    }); if (err != nil) {
+        fmt.Println("Uploading diffs has failed.")
+    }
+}
+
+func getDiffs() (string, error) {
+    currentDir := getCurrentDir()
+    repository := getRepository(currentDir)
+
+    diffs, err := getPatchDiffs(repository)
+    if err != nil {
+        panic(err)
+    }
+    if len(diffs) > 0 {
+        return diffs, nil
+    } else {
+        return "", errors.New("diff doesn't exist. No commit exists")
+    }
 }
