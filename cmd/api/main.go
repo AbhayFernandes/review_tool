e9ff368fa9e495b09e8345da4c72f7af86131055
Abhay <abhay.fernandes@protonmail.com>
2024-12-31 21:44:04 -0500

    feat: add uploading diffs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	"github.com/AbhayFernandes/review_tool/pkg/proto"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	log.Println("Starting API Service")
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalln("failed to create listener: ", err)
+		log.Fatalln("Failed to create listener: ", err)
 	}
 
 	s := grpc.NewServer()
-	// Make sure this is removed for prod!
-	reflection.Register(s)
 
-	proto.RegisterReviewServiceServer(s, &server{})
+    client, err := mongo.Connect(options.Client().ApplyURI("mongodb://db:27017"))
+    if (err != nil) {
+        log.Fatalln("Failed to connect to mongo")
+    }
+
+    if err := client.Ping(context.TODO(), nil); err != nil {
+        log.Fatalln("Could not connect to MongoDB: ", err)
+    }
+
+
+    defer func() {
+        if err = client.Disconnect(context.Background()); err != nil {
+            log.Fatalln("Failed to disconnect from mongo")
+            panic(err)
+        }
+    }()
+
+	proto.RegisterReviewServiceServer(s, &server{
+        proto.UnimplementedReviewServiceServer{},
+        client,
+    })
+
 	if err := s.Serve(listener); err != nil {
 		log.Fatalln("failed to serve:", err)
 	}
